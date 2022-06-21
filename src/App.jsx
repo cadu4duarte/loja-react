d4be54ad3e33e738615947408745459c9271760b
Carlos Eduardo <cadu4duarte@gmail.com>
2022-06-21 19:13:52 -0300

    chore: initialize project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import {BrowserRouter, Routes, Route } from "react-router-dom";
+import "./global.css";
+import { Header } from "./components/Header/header.jsx";
+import { Home } from "./pages/home";
+
+
+function App() {
+  
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header/>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App

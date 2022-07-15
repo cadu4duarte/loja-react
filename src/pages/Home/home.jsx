39bf356cc4a5a10eba18491607a45930efe02adc
Carlos Eduardo <cadu4duarte@gmail.com>
2022-07-14 21:47:03 -0300

    delete itens

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -19,12 +19,15 @@ export function Home() {
     loadProducts();
   }, []);
 
+
+  //filtrando os itens pela categoria e setando o resultado no productsCategory
   function filterProducts(cat) {
       const filtering = products.filter(item => item.category === cat)
       setProductsCategory(filtering)
       
   }
 
+  //resetando o filtro
   async function resetFilter() {
     const response = await api.get('https://fakestoreapi.com/products');
 
@@ -33,9 +36,27 @@ export function Home() {
     
   }
 
-
+  //trazendo os valores únicos das categorias
   const AllCategories = [ ...new Set(products.map(item => item.category))];
 
+  function addInCart(product) {
+    // console.log(product)
+
+    //getCart recebe os itens dentro do local storage e é um array vazio
+    let getCart = JSON.parse(localStorage.getItem("products")) || [];
+
+    //add novos itens no array
+    getCart.push(product);
+
+    //setando os itens do array no local storage
+    localStorage.setItem("products", JSON.stringify(getCart));
+    alert("Product set in cart");
+
+    
+    
+  }
+
+  
 
   return(
    <div className="container">
@@ -65,7 +86,9 @@ export function Home() {
             <img src={item.image}/>
           </div>
           <h3>{item.title}</h3>
+          <p>{item.id}</p>
           <p>{item.price}</p>
+          <button onClick={()=> addInCart(item)}>Buy</button>
         </div>
       )}
 
